0e6a3d65ec5f11272ba45b493345292fd250cbbf
sakuratail <sakuratail.2022@gmail.com>
2023-02-01 21:24:34 +0900

    update

--- public/script.js
+++ public/script.js
@@ -46,3 +46,22 @@ function getMyPlace() {
   };
   navigator.geolocation.getCurrentPosition(success, error);//成功と失敗を判断
 }
+
+function set2fig(num) {
+  // 桁数が1桁だったら先頭に0を加えて2桁に調整する
+  var ret;
+  if( num < 10 ) { ret = "0" + num; }
+  else { ret = num; }
+  return ret;
+}
+function showClock2() {
+  var nowTime = new Date();
+  var nowHour = set2fig( nowTime.getHours() );
+  var nowMin  = set2fig( nowTime.getMinutes() );
+  var nowSec  = set2fig( nowTime.getSeconds() );
+  var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
+  document.getElementById("RealtimeClockArea").innerHTML = msg;
+  var element3 = document.getElementById('time');
+  element3.value = nowHour + ":" + nowMin + ":" + nowSec;
+}
+setInterval('showClock2()',1000);
